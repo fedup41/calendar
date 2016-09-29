cf017cad0c89d892b618ae682599533229a10e18
Chris DeGreef <fedupforone@gmail.com>
2016-09-29 15:22:06 -0500

    timezone issue in test case

--- src/test/java/com/obdobion/calendar/JSONDateTest.java
+++ src/test/java/com/obdobion/calendar/JSONDateTest.java
@@ -34,7 +34,7 @@ public class JSONDateTest
     @Test
     public void basicConstructor() throws Exception
     {
-        final LocalDateTime ldt = CalendarFactory.modify("2013-03-11T01:38:18.309-0500");
+        final LocalDateTime ldt = CalendarFactory.modify("2013-03-11T01:38:18.309Z");
         Assert.assertEquals(
                 "=2013year =3month =11day =1hour =38minute =18second =309000000nanosecond",
                 CalendarFactory.asFormula(ldt));
@@ -46,6 +46,7 @@ public class JSONDateTest
         Assert.assertEquals("second", 18, ldt.getSecond());
         Assert.assertEquals("nanosecond", 309000000, ldt.getNano());
 
-        Assert.assertEquals("JSON format", "2013-03-11T01:38:18.309-05", CalendarFactory.asJSON(ldt));
+        final String jsonFormat = CalendarFactory.asJSON(ldt);
+        Assert.assertEquals("JSON format", "2013-03-11T01:38:18.309", jsonFormat.substring(0, 23));
     }
 }
